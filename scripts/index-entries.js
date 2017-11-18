e7fd3a50a31695e87394ba56a630646c241231d2
Jeremy Greer <jeremy.greer.atl@gmail.com>
2017-11-17 21:27:37 -0600

    add script to index entries for us

--- scripts/index-entries.js
+++ scripts/index-entries.js
@@ -1,3 +1,14 @@
-#!/usr/lib/bin node
+#!/usr/bin/env node
 
-console.log('yo')
+const fs = require('fs')
+const path = require('path')
+const dir = require('node-dir')
+const dirPath = path.resolve(__dirname, '../public')
+const regex = /\.txt$/
+const files = dir.files(dirPath, 'file', null, {
+  sync: true,
+  shortName: true,
+}).filter(name => name.match(regex))
+
+const outPath = path.resolve(__dirname, '../src/entries.json')
+fs.writeFileSync(outPath, JSON.stringify(files))
