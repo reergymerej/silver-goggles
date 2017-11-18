31c1362b90a2a9363d9af0a6de62bf0e3b9bd237
Jeremy Greer <jeremy.greer.atl@gmail.com>
2017-11-18 10:35:10 -0600

    start rendering entries

--- src/App.js
+++ src/App.js
@@ -3,18 +3,33 @@ import logo from './logo.svg';
 import './App.css';
 import entries from './entries.json'
 
+const Entry = () => (
+)
+
 class App extends Component {
+  state = {
+    currentEntry: null,
+  }
+
+  handleEntrySelected = (entry) => {
+    this.setState({ currentEntry: entry })
+  }
+
   render() {
     return (
       <div className="App">
         {
-          entries.map(name => (
+          entries.map(entry => (
             <a
-              key={name}
-              href={`/${name}`}
-              >{name}</a>
+              key={entry.name}
+              href="#"
+              onPress={this.handleEntrySelected.bind(this, entry)}
+              >{entry.name}</a>
           ))
         }
+        <div>
+          <Entry entry={this.state.currentEntry} />
+        </div>
       </div>
     );
   }
