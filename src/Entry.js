35a23a29d3ab13a2fbb07a509b0855e09581f654
Jeremy Greer <jeremy.greer.atl@gmail.com>
2017-11-18 16:15:03 -0600

    dummy rendering parsed commentary

--- src/Entry.js
+++ src/Entry.js
@@ -1,5 +1,39 @@
 import React from 'react'
 
+const parseCommentary = (commentary) => {
+  return [
+    0,
+    1,
+    2,
+  ]
+}
+
+const TextBlock = (props) => (
+  <pre>
+    {props.value}
+  </pre>
+)
+
+const GitRefBlock = () => (
+  <a href="asdf/asdf">a Git reference</a>
+)
+
+const ParsedCommentary = (props) => {
+  const parsedCommentaryBlocks = parseCommentary(props.commentary)
+  return (
+    <div>
+      {
+        parsedCommentaryBlocks.map(block => {
+          if (block % 2 === 0) {
+            return <TextBlock />
+          }
+          return <GitRefBlock />
+        })
+      }
+    </div>
+  )
+}
+
 const Entry = (props) => {
   const { terminal } = props.entry.resources
   const terminalUri = terminal && terminal.uri
@@ -11,17 +45,7 @@ const Entry = (props) => {
           <a href={terminalUri}>terminal.log</a>
         }
       </div>
-      <pre>
-        {props.entry.commentary}
-      </pre>
-      <a href="asdf/asdf">a Git reference</a>
-      <pre>
-        {props.entry.commentary}
-      </pre>
-      <a href="asdf/asdf">a Git reference</a>
-      <pre>
-        {props.entry.commentary}
-      </pre>
+      <ParsedCommentary commentary={props.entry.commentary} />
     </div>
   )
 }
