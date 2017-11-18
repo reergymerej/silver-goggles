6332a4acb1cf72cd2f6e990fd8fd020470a227a4
Jeremy Greer <jeremy.greer.atl@gmail.com>
2017-11-17 21:33:18 -0600

    dynamically render links to entries

--- src/App.js
+++ src/App.js
@@ -1,14 +1,20 @@
 import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
+import entries from './entries.json'
 
 class App extends Component {
   render() {
     return (
       <div className="App">
-        <a href="/01.txt">01.txt</a>
-        <a href="/02.txt">02.txt</a>
-        <a href="/03.txt">03.txt</a>
+        {
+          entries.map(name => (
+            <a
+              key={name}
+              href={`/${name}`}
+              >{name}</a>
+          ))
+        }
       </div>
     );
   }
