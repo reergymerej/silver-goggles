6b4a5e0edce1d09d12ad0452c1a09e16820398cd
Jeremy Greer <jeremy.greer.atl@gmail.com>
2017-11-19 17:49:39 -0600

    show terminal log when link is clicked

--- src/components/Entry.js
+++ src/components/Entry.js
@@ -1,8 +1,19 @@
 import React from 'react'
 import { TYPE } from '../constants'
 import ParsedCommentary from './ParsedCommentary'
+import './Entry.css'
 
 class Entry extends React.Component {
+  state = {
+    showTerminalLog: false,
+  }
+
+  componentWillReceiveProps(nextProps) {
+    if (nextProps.entry !== this.props.entry) {
+      this.setState({ showTerminalLog: false })
+    }
+  }
+
   handleCommitSelected = value => {
     // FIXME: This looks like we're recreating the resource structure.
     this.props.onResourceSelected({
@@ -11,22 +22,46 @@ class Entry extends React.Component {
     })
   }
 
+  handleTerminalLogClick = (event) => {
+    event.preventDefault()
+    this.setState({ showTerminalLog: true })
+  }
+
+  renderTerminalLog() {
+    const { content } = this.props.entry.resources.terminal
+    return (
+      <div>
+        {content}
+      </div>
+    )
+  }
+
   render() {
     const { props } = this
     const { terminal } = props.entry.resources
     const terminalUri = terminal && terminal.uri
     return (
-      <div>
+      <div className="Entry">
         <h2>{props.entry.name}</h2>
-        <div>
-          { terminalUri &&
-            <a href={terminalUri}>terminal.log</a>
-          }
+        <div className="content">
+          <div className="left">
+            <div>
+              { terminalUri &&
+                <a
+                  href={terminalUri}
+                  onClick={this.handleTerminalLogClick}
+                >terminal.log</a>
+              }
+            </div>
+            <ParsedCommentary
+              commentary={props.entry.commentary}
+              onCommitSelected={this.handleCommitSelected}
+            />
+          </div>
+          <pre className="right">
+            { this.state.showTerminalLog && this.renderTerminalLog() }
+          </pre>
         </div>
-        <ParsedCommentary
-          commentary={props.entry.commentary}
-          onCommitSelected={this.handleCommitSelected}
-        />
       </div>
     )
   }
