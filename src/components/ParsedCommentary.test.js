2283e88cd1ec4dce9d238a46281db0770ed2e8e5
Jeremy Greer <jeremy.greer.atl@gmail.com>
2017-11-19 10:26:09 -0600

    test that a text block will come through

--- src/components/ParsedCommentary.test.js
+++ src/components/ParsedCommentary.test.js
@@ -6,5 +6,7 @@ describe('<ParsedCommentary />', () => {
   it('should render <TextBlock />', () => {
     const commentary = 'blah blah blah'
     const wrapper = shallow(<ParsedCommentary commentary={commentary} />)
+    const comp = wrapper.find('TextBlock')
+    expect(comp.length).toBe(1)
   })
 })
