455ae121b8405a81985906d810e003cb07d3dddf
Jeremy Greer <jeremy.greer.atl@gmail.com>
2017-11-19 10:31:59 -0600

    test git references are rendered

--- src/components/ParsedCommentary.test.js
+++ src/components/ParsedCommentary.test.js
@@ -9,4 +9,17 @@ describe('<ParsedCommentary />', () => {
     const comp = wrapper.find('TextBlock')
     expect(comp.length).toBe(1)
   })
+
+  it('should render <GitRefBlock />', () => {
+    const commentary = `
+    blah blah blah
+
+    ref:git:2283e88
+
+    blah blah blah
+    `
+    const wrapper = shallow(<ParsedCommentary commentary={commentary} />)
+    const comp = wrapper.find('GitRefBlock')
+    expect(comp.length).toBe(1)
+  })
 })
